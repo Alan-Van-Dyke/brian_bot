94947dad8442be9f20c20312cb34b12eca4d377a
Van Dyke <vandykea@msoe.edu>
2022-11-02 11:43:24 -0500

    prelim embed work

--- commands/todo/showtodo.js
+++ commands/todo/showtodo.js
@@ -17,8 +17,26 @@ module.exports = {
 
         await guildProfile.save().catch(console.error);
 
+        const embed = new Discord.EmbedBuilder()
+            .setColor(0x00FF00)
+            .setTitle(guildProfile.guildName + " Todo List")
+            .setFields({name: "ID", value: "\u200b", inline: true},
+                       {name: "Content", value: "\u200b", inline: true},
+                       {name: "Date", value: "\u200b", inline:true})
+        
+        guildProfile.todoItems.forEach((item) => {
+            const dateString = item.remindDate.getMonth() + "/" + item.remindDate.getDay() + " at " + item.remindDate.getHours() + ":" + item.remindDate.getMinutes()
+
+            console.log(item.idNum)
+            embed.addFields({name: "\u200b", value: item.idNum + "", inline:true},
+                            {name: "\u200b", value: item.text, inline:true},
+                            {name: "\u200b", value: dateString, inline:true})
+        })
+
+        console.log(interaction.channel.send({embeds: [embed]}))
+
         await interaction.reply({
-            content: "List" + guildProfile.todoItems
+            content: "done!"
         })
     },
 };
